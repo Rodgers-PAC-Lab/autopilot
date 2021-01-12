620774a3539700844e1c70317c5ed1621591b1cb
Chris Rodgers <xrodgers@gmail.com>
2021-01-12 17:39:32 -0500

    fix

--- autopilot/stim/sound/jackclient.py
+++ autopilot/stim/sound/jackclient.py
@@ -257,12 +257,13 @@ class JackClient(mp.Process):
                 #self.client.outports[0].get_array()[:] = data.T
                 
                 #print("len outports: {}".format(len(self.client.outports)))
-                #print("data shape: {}".format(data.shape))
-                buff = self.client.outports[0].get_array()
+                print("data shape: {}".format(data.shape))
+                buff0 = self.client.outports[0].get_array()
+                buff1 = self.client.outports[1].get_array()
                 
                 #print("buff shape: {}".format(buff.shape))
-                self.client.outports[0].get_array()[:] = data[:, 0]
-                self.client.outports[1].get_array()[:] = data[:, 1]
+                buff0[:] = data[:, 0]
+                buff1[:] = data[:, 1]
 
                 # if not self.continuous_started:
                 #     # if we are just entering continuous mode, get the continuous sound and prepare to play it
