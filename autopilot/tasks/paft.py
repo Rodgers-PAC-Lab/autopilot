2c3a1514783928d561cbada6484d4df8f003f2ec
PAC Lab <rodgers.paclab@gmail.com>
2022-04-05 15:48:04 -0400

    update

--- autopilot/tasks/paft.py
+++ autopilot/tasks/paft.py
@@ -369,7 +369,7 @@ class PAFT(Task):
                 'pilot': prefs.get('NAME'),
                 'continuous': True,
                 'poked_port': 'L',
-                'poked_pilot': 'rpi03',
+                'poked_pilot': 'rpi10',
                 'poked_int': 3,
                 'timestamp': datetime.datetime.now().isoformat(),
                 },
