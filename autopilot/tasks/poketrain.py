55568883cc855b249aea885749515456b13a8b0c
PAC lab <rodgers.paclab@gmail.com>
2022-08-03 14:56:53 -0400

    update autopilot.core.loggers to autopilot.utils.loggers

--- autopilot/tasks/poketrain.py
+++ autopilot/tasks/poketrain.py
@@ -23,7 +23,7 @@ from autopilot.tasks.task import Task
 from autopilot.networking import Net_Node
 from autopilot import prefs
 from autopilot.hardware import BCM_TO_BOARD
-from autopilot.core.loggers import init_logger
+from autopilot.utils.loggers import init_logger
 from autopilot.stim.sound import jackclient
 
 
