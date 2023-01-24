8c6de2de2ea26b9e59544727ebdeff39ada374f3
PAC Lab <rodgers.paclab@gmail.com>
2023-01-24 18:09:48 -0500

    stubs for test_poke_colors not working

---
+++ autopilot/tasks/test_poke_colors.py
@@ -0,0 +1,430 @@
+"""This module defines Paft_With_Children_Pokes task.
+
+This is a stripped-down task that only receives information about
+pokes from each child.
+"""
+
+import threading
+import itertools
+import random
+import datetime
+import functools
+from collections import OrderedDict as odict
+import time
+import queue
+import tables
+import numpy as np
+import pandas
+import autopilot.hardware.gpio
+from autopilot.stim.sound import sounds
+from autopilot.tasks.task import Task
+from autopilot.networking import Net_Node
+from autopilot import prefs
+from autopilot.hardware import BCM_TO_BOARD
+from autopilot.utils.loggers import init_logger
+from autopilot.stim.sound import jackclient
+
+# The name of the task
+# This declaration allows Subject to identify which class in this file 
+# contains the task class. 
+TASK = 'Paft_With_Children_Pokes'
+
+
+## Set box-specific params
+# TODO: Figure out some cleaner way of doing this
+# But right now vars like MY_PI2 are needed just to initiate a PAFT object
+
+# Figure out which box we're in
+MY_NAME = prefs.get('NAME')
+
+if MY_NAME in ['rpi01', 'rpi02', 'rpi03', 'rpi04']:
+    MY_BOX = 'Box1'
+    MY_PARENTS_NAME = 'rpi01'
+    MY_PI1 = 'rpi01'
+    MY_PI2 = 'rpi02'
+    MY_PI3 = 'rpi03'
+    MY_PI4 = 'rpi04'
+
+elif MY_NAME in ['rpi05', 'rpi06', 'rpi07', 'rpi08']:
+    MY_BOX = 'Box2'
+    MY_PARENTS_NAME = 'rpi05'
+    MY_PI1 = 'rpi05'
+    MY_PI2 = 'rpi06'
+    MY_PI3 = 'rpi07'
+    MY_PI4 = 'rpi08'
+
+elif MY_NAME in ['rpi09', 'rpi10', 'rpi11', 'rpi12']:
+    MY_BOX = 'Box2'
+    MY_PARENTS_NAME = 'rpi09'
+    MY_PI1 = 'rpi09'
+    MY_PI2 = 'rpi10'
+    MY_PI3 = 'rpi11'
+    MY_PI4 = 'rpi12'
+
+else:
+    # This happens on the Terminal, for instance
+    MY_BOX = 'NoBox'
+    MY_PARENTS_NAME = 'NoParent'
+    MY_PI1 = 'NoPi1'
+    MY_PI2 = 'NoPi2'
+    MY_PI3 = 'NoPi3'
+    MY_PI4 = 'NoPi4'
+
+
+## Define the Task
+class Paft_With_Children_Pokes(Task):
+    """The probabalistic auditory foraging task (PAFT).
+    
+    This task chooses a port at random, lights up the LED for that port,
+    plays sounds through the associated speaker, and then dispenses water 
+    once the subject pokes there.
+
+    Stage list:
+    * waiting for the response
+    * reporting the response
+
+    Class attributes:
+        PARAMS : collections.OrderedDict
+            This defines the params we expect to receive from the terminal.
+        DATA : dict of dicts
+            This defines the kind of data we return to the terminal
+        TrialData : subclass of tables.IsDescription
+            This defines how to set up the hdf5 file for the Subject with
+            the returned data
+        HARDWARE : dict of dicts
+            Defines 'POKES', 'PORTS', and 'LEDS'
+        CHILDREN : dict of dicts
+            Defines the child pis that we'll connect to
+        PLOT : dict of dicts
+            Defines how to plot the results
+
+    Attributes:
+        target ('L', 'C', 'R'): The correct port
+        trial_counter (:class:`itertools.count`): 
+            Counts trials starting from current_trial specified as argument
+        triggers (dict): 
+            Dictionary mapping triggered pins to callable methods.
+        num_stages (int): 
+            number of stages in task (2)
+        stages (:class:`itertools.cycle`): 
+            iterator to cycle indefinitely through task stages.
+    """
+    
+    
+    ## Define the class attributes
+    # This defines params we receive from terminal on session init
+    PARAMS = odict()
+    PARAMS['reward'] = {
+        'tag':'Reward Duration (ms)',
+        'type':'int',
+        }
+
+    # This defines the data we return after each trial
+    DATA = {
+        'trial': {'type':'i32'},
+        'trials_total': {'type': 'i32'},
+        'rpi': {'type': 'S10'},
+        'side': {'type': 'S10'},
+        'light': {'type': 'S10'},
+        'sound': {'type': 'S10'},
+        'timestamp': {'type':'S26'},
+    }
+
+    # This is used by the terminal to build an HDF5 file of data for each trial 
+    class TrialData(tables.IsDescription):
+        # The trial within this session
+        trial = tables.Int32Col()
+        
+        # The trial number accumulated over all sessions
+        trials_total = tables.Int32Col()
+        
+        # The target
+        rpi = tables.StringCol(10)
+        side = tables.StringCol(10)
+        light = tables.StringCol(10)
+        sound = tables.StringCol(10)
+        
+        # The timestamp
+        timestamp = tables.StringCol(26)
+
+    # This defines the hardware that is required
+    HARDWARE = {
+        'POKES':{
+            'L': autopilot.hardware.gpio.Digital_In,
+            'R': autopilot.hardware.gpio.Digital_In
+        },
+        'LEDS':{
+            'L': autopilot.hardware.gpio.LED_RGB,
+            'R': autopilot.hardware.gpio.LED_RGB
+        },
+        'PORTS':{
+            'L': autopilot.hardware.gpio.Solenoid,
+            'R': autopilot.hardware.gpio.Solenoid
+        }
+    }
+    
+    # This defines the child rpis to connect to
+    CHILDREN = {
+        MY_PI2: {
+            'task_type': "Paft_With_Children_Pokes_Child",
+        },
+        MY_PI3: {
+            'task_type': "Paft_With_Children_Pokes_Child",
+        },
+        MY_PI4: {
+            'task_type': "Paft_With_Children_Pokes_Child",
+        },
+    }
+    
+    # This is used by the terminal to plot the results of each trial
+    PLOT = {
+        'data': {
+            'target': 'point'
+        }
+    }
+    
+    
+    ## Define the class methods
+    def __init__(self, stage_block, current_trial, step_name, task_type, 
+        subject, step, session, pilot, graduation, reward):
+        """Initialize a new PAFT Task. 
+        
+        
+        --
+        Plan
+        
+        The first version of this new task needs to:
+        * Be able to play streams of sounds from two speakers (no children)
+        
+        Another version needs to
+        * Connect to the children, without playing sounds
+        
+        Then these can be merged in a second version that can
+        * Report pokes from children
+        * Tell children to play sounds
+        
+        The third version needs to:
+        * Incorporate advancing through stages
+        * Return data about each trial
+        
+        The fourth version needs to:
+        * Incorporate subject-specific training params
+        
+        
+        ---
+        
+        All arguments are provided by the Terminal.
+        
+        Note that this __init__ does not call the superclass __init__, 
+        because that superclass Task inclues functions for punish_block
+        and so on that we don't want to use.
+        
+        Some params, such as `step_name` and `task_type`, are always required 
+        to be specified in the json defining this protocol
+        
+        Other params, such as `reward`, are custom to this particular task.
+        They should be described in the class attribute `PARAMS` above, and
+        their values should be specified in the protocol json.
+        
+        Arguments
+        ---------
+        stage_block (:class:`threading.Event`): 
+            used to signal to the carrying Pilot that the current trial 
+            stage is over
+        current_trial (int): 
+            If not zero, initial number of `trial_counter`
+        step_name : string
+            This is passed from the "protocol" json
+            Currently it is always "PAFT"
+        task_type : string
+            This is passed from the "protocol" json
+            Currently it is always "PAFT"
+        subject : string
+            The name of the subject
+        step : 0
+            Index into the "protocol" json?
+        session : int
+            number of times it's been started
+        pilot : string
+            The name of this pilot
+        graduation : dict
+            Probably a dict of graduation criteria
+        reward (int): 
+            ms to open solenoids
+            This is passed from the "protocol" json
+        """    
+        
+        ## These are things that would normally be done in superclass __init__
+        # Set up a logger first, so we can debug if anything goes wrong
+        self.logger = init_logger(self)
+
+        # Use the provided threading.Event to handle stage progression
+        self.stage_block = stage_block
+        
+        # This is used to count the trials, it is initialized by
+        # the Terminal to wherever we are in the Protocol graduation
+        self.trial_counter = itertools.count(int(current_trial))        
+
+        # A dict of hardware triggers
+        self.triggers = {}
+        
+    
+        ## Define the stages
+        # Stage list to iterate
+        stage_list = [self.play]
+        self.num_stages = len(stage_list)
+        self.stages = itertools.cycle(stage_list)        
+        
+        
+        ## Init hardware -- this sets self.hardware and self.pin_id
+        self.init_hardware()
+
+
+        ## Connect to children
+        # This dict keeps track of which self.CHILDREN have connected
+        self.child_connected = {}
+        for child in self.CHILDREN.keys():
+            self.child_connected[child] = False
+        
+        # Tell each child to start the task
+        self.initiate_task_on_children(subject, reward)
+        
+        # Create a Net_Node for communicating with the children, and
+        # wait until all children have connected
+        self.create_inter_pi_communication_node()
+
+    def initiate_task_on_children(self, subject, reward):
+        """Defines a Net_Node and uses it to tell each child to start
+        
+        This Net_Node is saved as `self.node`. A 'CHILD' message is sent,
+        I think to the Pilot_Node, which is handled by
+        networking.station.Pilot_Node.l_child .
+        
+        That code broadcasts the 'START' message to each of the children
+        specified in CHILDID in prefs.json, telling them to start the
+        'PAFT_Child' task. That 'START' message also includes task 
+        parameters specified here, such as subject name and reward value.
+        
+        The same `self.node` is used later to end the session on the children.
+        """
+        # With instance=True, I get a threading error about current event loop
+        self.node = Net_Node(
+            id="T_{}".format(prefs.get('NAME')),
+            upstream=prefs.get('NAME'),
+            port=prefs.get('MSGPORT'),
+            listens={},
+            instance=False,
+            )
+
+        # Construct a message to send to child
+        # Specify the subjects for the child (twice)
+        value = {
+            'child': {
+                'parent': prefs.get('NAME'), 'subject': subject},
+            'task_type': 'Paft_With_Children_Pokes_Child',
+            'subject': subject,
+            'reward': reward,
+        }
+
+        # send to the station object with a 'CHILD' key
+        self.node.send(to=prefs.get('NAME'), key='CHILD', value=value)        
+
+    def create_inter_pi_communication_node(self):
+        """Defines a Net_Node to communicate with the children
+        
+        This is a second Net_Node that is used to directly exchange information
+        with the children about pokes and sounds. Unlike the first Net_Node,
+        for this one the parent is the "router" / server and the children
+        are the "dealer" / clients .. ie many dealers, one router.
+        
+        Each child needs to create a corresponding Net_Node and connect to
+        this one. This function will block until that happens.
+        
+        The Net_Node defined here also specifies "listens" (ie triggers)
+        of functions to be called upon receiving specified messages
+        from the children, such as "HELLO" or "POKE".
+        
+        This Net_Node is saved as `self.node2`.
+        """
+        ## Create a second node to communicate with the child
+        # We (parent) are the "router"/server
+        # The children are the "dealer"/clients
+        # Many dealers, one router        
+        self.node2 = Net_Node(
+            id='parent_pi',
+            upstream='',
+            port=5000,
+            router_port=5001,
+            listens={
+                'HELLO': self.recv_hello,
+                'POKE': self.recv_poke,
+                },
+            instance=False,
+            )
+
+        # Wait until the child connects!
+        self.logger.debug("Waiting for child to connect")
+        while True:
+            stop_looping = True
+            
+            for child, is_conn in self.child_connected.items():
+                if not is_conn:
+                    stop_looping = False
+            
+            if stop_looping:
+                break
+        self.logger.debug(
+            "All children have connected: {}".format(self.child_connected))
+
+    def play(self):
+        # Sleep so we don't go crazy
+        time.sleep(3)
+        
+        # Continue to the next stage (which is this one again)
+        self.stage_block.set()
+
+    def init_hardware(self):
+        """
+        Use the HARDWARE dict that specifies what we need to run the task
+        alongside the HARDWARE subdict in :mod:`prefs` to tell us how
+        they're plugged in to the pi
+
+        Instantiate the hardware, assign it :meth:`.Task.handle_trigger`
+        as a callback if it is a trigger.
+        
+        Sets the following:
+            self.hardware
+            self.pin_id
+        """
+        # We use the HARDWARE dict that specifies what we need to run the task
+        # alongside the HARDWARE subdict in the prefs structure to tell us 
+        # how they're plugged in to the pi
+        self.hardware = {}
+
+    def recv_hello(self, value):
+        self.logger.debug(
+            "received HELLO from child with value {}".format(value))
+        
+        # Set this flag
+        self.child_connected[value['from']] = True
+    
+    def recv_poke(self, value):
+        self.logger.debug(
+            "received POKE from child with value {}".format(value))
+
+    def end(self):
+        """
+        When shutting down, release all hardware objects and turn LEDs off.
+        """
+        self.logger.debug('inside self.end')
+        
+        # Tell the child to end the task
+        self.node.send(to=prefs.get('NAME'), key='CHILD', value={'KEY': 'STOP'})
+
+        # This sock.close seems to be necessary to be able to communicate again
+        self.node.sock.close()
+        self.node.release()
+        
+        # This router.close() prevents ZMQError on the next start
+        self.node2.router.close()
+        self.node2.release()
