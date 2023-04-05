7b3b5dc62796a647a7ceb337ace9dd466f6170d2
PAC lab <rodgers.paclab@gmail.com>
2023-04-05 19:15:33 -0400

    register a new chunk type

--- autopilot/tasks/paft_startle.py
+++ autopilot/tasks/paft_startle.py
@@ -115,7 +115,7 @@ class PAFT_startle(Task):
     
     # This defines the classes that act like ChunkData
     # See Subject.data_thread
-    CHUNKDATA_CLASSES = [ChunkData_Sounds, ChunkData_Pokes]
+    CHUNKDATA_CLASSES = [ChunkData_Sounds, ChunkData_SoundsPlayed, ChunkData_Pokes]
     
     ## Set up hardware and children
     # Per https://docs.auto-pi-lot.com/en/latest/guide/task.html:
