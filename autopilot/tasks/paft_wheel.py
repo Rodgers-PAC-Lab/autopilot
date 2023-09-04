d0c919e4fcdb7714d7ef358a9dd61800c0b6802b
Chris Rodgers <xrodgers@gmail.com>
2023-09-04 16:45:10 -0400

    new wheel task

---
+++ autopilot/tasks/paft_wheel.py
@@ -0,0 +1,124 @@
+"""This module defines the PAFT_Wheel task
+
+Take input from a rotary encoder
+"""
+
+import threading
+import itertools
+import random
+import datetime
+import functools
+from collections import OrderedDict as odict
+import tables
+import numpy as np
+import pandas
+import autopilot.hardware.gpio
+from autopilot.stim.sound import sounds
+from autopilot.tasks.task import Task
+from autopilot.networking import Net_Node
+from autopilot import prefs
+from autopilot.hardware import BCM_TO_BOARD
+from autopilot.utils.loggers import init_logger
+
+# The name of the task
+# This declaration allows Subject to identify which class in this file 
+# contains the task class. 
+TASK = 'PAFT_Wheel'
+
+
+## Set box-specific params
+# TODO: Figure out some cleaner way of doing this
+# But right now vars like MY_PI2 are needed just to initiate a PAFT object
+
+
+## Define the Task
+class PAFT_Wheel(Task):
+
+    ## Define the class attributes
+    # This defines params we receive from terminal on session init
+    PARAMS = odict()
+    PARAMS['reward'] = {
+        'tag':'Reward Duration (ms)',
+        'type':'int',
+        }
+
+    
+    ## Define the class methods
+    def __init__(self, stage_block, current_trial, step_name, task_type, 
+        subject, step, session, pilot, graduation, reward):
+        """Initialize a new MinimalPAFT Task. 
+        
+        All arguments are provided by the Terminal.
+        
+        Note that this __init__ does not call the superclass __init__, 
+        because that superclass Task inclues functions for punish_block
+        and so on that we don't want to use.
+        
+        Some params, such as `step_name` and `task_type`, are always required 
+        to be specified in the json defining this protocol
+        
+        Other params, such as `reward`, are custom to this particular task.
+        They should be described in the class attribute `PARAMS` above, and
+        their values should be specified in the protocol json.
+        
+        Arguments
+        ---------
+        stage_block (:class:`threading.Event`): 
+            used to signal to the carrying Pilot that the current trial 
+            stage is over
+        current_trial (int): 
+            If not zero, initial number of `trial_counter`
+        step_name : string
+            This is passed from the "protocol" json
+            Currently it is always "PAFT"
+        task_type : string
+            This is passed from the "protocol" json
+            Currently it is always "PAFT"
+        subject : string
+            The name of the subject
+        step : 0
+            Index into the "protocol" json?
+        session : int
+            number of times it's been started
+        pilot : string
+            The name of this pilot
+        graduation : dict
+            Probably a dict of graduation criteria
+        reward (int): 
+            ms to open solenoids
+            This is passed from the "protocol" json
+        """    
+        
+        ## These are things that would normally be done in superclass __init__
+        # Set up a logger first, so we can debug if anything goes wrong
+        self.logger = init_logger(self)
+
+        # Use the provided threading.Event to handle stage progression
+        self.stage_block = stage_block
+        
+        # This is used to count the trials, it is initialized by
+        # the Terminal to wherever we are in the Protocol graduation
+        self.trial_counter = itertools.count(int(current_trial))        
+
+        # A dict of hardware triggers
+        self.triggers = {}
+    
+    
+        ## Define the stages
+        # Stage list to iterate
+        stage_list = [self.do_nothing]
+        self.num_stages = len(stage_list)
+        self.stages = itertools.cycle(stage_list)        
+        
+        
+        ## Init hardware -- this sets self.hardware and self.pin_id
+        self.init_hardware()
+
+    def do_nothing(self):
+        print("test")
+        time.sleep(1)
+        
+        self.stage_block.set()
+
+    def init_hardware(self):
+        self.hardware = {}
